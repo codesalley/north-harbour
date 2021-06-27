a32cf8f4a3dc819215c628c00d338b45ecda78cc
juxsalley <juxsalley@gmail.com>
2021-06-27 16:32:38 +0000

    add login and sign up mobile ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+import "../stylesheets/application"
+import 'bootstrap/dist/js/bootstrap'
+import "bootstrap/dist/css/bootstrap";
+
 
 Rails.start()
 Turbolinks.start()

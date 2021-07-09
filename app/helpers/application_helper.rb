be2a3c43330de379da4140e94b1525fefaa94795
juxsalley <juxsalley@gmail.com>
2021-07-09 14:00:15 +0000

    add gravatar using user email and default photo incase none is set

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    def avatar_url_for(user, opts = {})
+        size = opts[:size || 20]
+    
+        if user.avatar.attached?
+          user.avatar.variant(
+            resize: "#{size}x#{size}!"
+          )
+        else
+          
+          "https://res.cloudinary.com/north-harbour-gh/image/upload/v1625838211/content/AvatarMaker_1_mti9vl.png"
+
+        end
+     end
 end
